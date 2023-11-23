a364116ecfded5cf4433dd6a1b5ba85c7aa6f3dd
Hugo Vacquier <94376539+Hugo-Vacquier@users.noreply.github.com>
2023-11-23 10:31:06 +0100

    FizzBuzz amélioré

--- src/Fizz/FizzBuzz.java
+++ src/Fizz/FizzBuzz.java
@@ -1,7 +1,7 @@
 package Fizz;
 
 public class FizzBuzz {
-	public static String fizzBuzz(int number) {
+	/*public static String fizzBuzz(int number) {
         if (number % 3 == 0 && number % 5 == 0) {
             return "FizzBuzz";
         } else if (number % 3 == 0) {
@@ -11,5 +11,20 @@ public class FizzBuzz {
         } else {
             return Integer.toString(number);
         }
-    }
+    }*/
+	
+	public static String fizzBuzz(int num) {
+		for (int i = 0; i<num; i++) {
+			if (num % 3 == 0 && num % 5 == 0) {
+	            return "FizzBuzz";
+	        } else if (num % 3 == 0) {
+	            return "Fizz";
+	        } else if (num % 5 == 0) {
+	            return "Buzz";
+	        } else {
+	            return Integer.toString(num);
+	        } 
+		}
+		return "num";	
+	}
 }
